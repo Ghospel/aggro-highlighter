9a5a8764b75e8cc6713b33c14de4be99f765f36f
Jasper Huizing <jasper.huizing@wearetriple.com>
2023-01-24 19:57:32 +0100

    it kinda works

--- src/main/java/com/huib/aggrohighlighter/AggroHighlighterPlugin.java
+++ src/main/java/com/huib/aggrohighlighter/AggroHighlighterPlugin.java
@@ -6,18 +6,15 @@ import javax.inject.Inject;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.NPC;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.NpcSpawned;
+import net.runelite.api.*;
+import net.runelite.api.events.*;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.npcoverlay.HighlightedNpc;
 import net.runelite.client.game.npcoverlay.NpcOverlayService;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,21 +37,28 @@ public class AggroHighlighterPlugin extends Plugin
 
 	@Getter(AccessLevel.PACKAGE)
 	private final Map<NPC, HighlightedNpc> highlightedNpcs = new HashMap<>();
+	private final Map<NPC, HighlightedNpc> notHighlightedNpcs = new HashMap<>();
+
+	private final Map<Actor, NPC> actorNPCMap = new HashMap<>();
 
 	private final Function<NPC, HighlightedNpc> isHighlighted = highlightedNpcs::get;
+
+	private Player localPlayer;
 
 	@Override
 	protected void startUp() throws Exception
 	{
 		npcOverlayService.registerHighlighter(isHighlighted);
-		log.info("Aggro Highlighter started!");
+		log.info("Aggro Highlighter started");
+		localPlayer = client.getLocalPlayer();
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
+		highlightedNpcs.clear();
 		npcOverlayService.unregisterHighlighter(isHighlighted);
-		log.info("Aggro Highlighter stopped!");
+		log.info("Aggro Highlighter stopped");
 	}
 
 	@Subscribe
@@ -65,6 +69,31 @@ public class AggroHighlighterPlugin extends Plugin
 		{
 			highlightedNpcs.clear();
 		}
+	}
+
+	@Subscribe
+	public void onAnimationChanged(AnimationChanged e)
+	{
+		if(isInteractingWithLocalPlayer(e.getActor())){
+			NPC npc = actorNPCMap.get(e.getActor());
+			if(hasAttackOption(npc))
+			{
+				log.info(e.getActor().getName() + " is attacking you");
+				log.info("Highlighting npc: " + npc.getName());
+				highlightedNpcs.put(npc, highlightedNpc(npc));
+				log.info("highlightedNPC count: " + highlightedNpcs.size());
+				npcOverlayService.registerHighlighter(isHighlighted);
+			}
+		}
+	}
+
+	private boolean isInteractingWithLocalPlayer(Actor e){
+		return e.getInteracting().getName().equals(client.getLocalPlayer().getName());
+	}
+
+	private boolean hasAttackOption(NPC npc)
+	{
+		return ArrayUtils.contains(npc.getTransformedComposition().getActions(), "Attack");
 	}
 
 	@Subscribe
@@ -79,7 +108,14 @@ public class AggroHighlighterPlugin extends Plugin
 		}
 
 		log.info("NPC Spawned: " + npc.getName());
-		highlightedNpcs.put(npc, highlightedNpc(npc));
+		actorNPCMap.put(npcSpawned.getActor(), npcSpawned.getNpc());
+	}
+	public void onNpcDespawned(NpcDespawned npcDespawned)
+	{
+		final NPC npc = npcDespawned.getNpc();
+		if(highlightedNpcs.containsKey(npc)){
+			highlightedNpcs.remove(npc);
+		}
 	}
 
 	@Provides
@@ -93,8 +129,24 @@ public class AggroHighlighterPlugin extends Plugin
 		return HighlightedNpc.builder()
 				.npc(npc)
 				.highlightColor(config.aggroHighLightColor())
+				.outline(true)
 				.borderWidth((float) config.borderWidth())
-				.render(x -> true)
+				.render(this::render)
 				.build();
 	}
+
+
+	private boolean render(NPC n)
+	{
+		return true;
+	}
+	
+//	private HighlightedNpc highlightedNpc(NPC npc)
+//	{
+//		return HighlightedNpc.builder()
+//				.npc(npc)
+//				.highlightColor(config.aggroHighLightColor())
+//				.borderWidth((float)config.borderWidth())
+//				.build();
+//	}
 }
